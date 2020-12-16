3d4fa018dc7cafaeb75159e6c62e5a611b927c34
zgersan <33616350+zgersan@users.noreply.github.com>
2020-12-16 22:04:21 +0300

    basurl link

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,4 +1,4 @@
 // const baseUrl = process.env.NODE_ENV === 'production' ? "https://mezzaneen.com": "http://localhost:3000"
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://mini-repo-demo.vercel.app": "http://localhost:3000"
+const baseUrl = process.env.NODE_ENV === 'production' ? "https://mini-repo-demo-enjxqaj2u.vercel.app": "http://localhost:3000"
 
 export default baseUrl
